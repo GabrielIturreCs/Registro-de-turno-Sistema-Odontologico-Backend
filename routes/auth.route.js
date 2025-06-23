241b55257ca1fea36a63026d68f55f42c0b78bbf
smrodriguez017 <39234551@fi.unju.edu.ar>
2025-06-23 20:46:29 -0300

    crear auth.route.js y ruta de el metodo registerUsuario

---
+++ routes/auth.route.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router(); 
+
+//defino controlador para el manejo de CRUD 
+const authCtrl = require('../controllers/auth.controller'); 
+
+// definiendo rutas 
+router.post('/', authCtrl.registerUsuario); 
+//router.post('/login', authCtrl.loginUsuario); 
+
+//exportacion del modulo de rutas 
+module.exports = router;
