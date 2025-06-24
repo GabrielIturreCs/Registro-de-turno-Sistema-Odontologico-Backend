be0fe22469df529e6c817b4f56913aad25fe52bd
smrodriguez017 <39234551@fi.unju.edu.ar>
2025-06-23 22:30:31 -0300

    Mejoras en configuración de servidor:  liempieza de imports y soporte para variable de entorno PORT

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const cors = require('cors')
-const {mongoose} = require('./database.js')
+const {mongoose} = require('./database')
 
 var app = express();
 
 app.use(express.json());
 app.use(cors({origin: 'http://localhost:4200'}));
  // cargar los modulos de routes
- app.use('/api/usuario', require('./routes/auth.route.js'));
+app.use('/api/usuario', require('./routes/auth.route.js'));
 
- app.set('port', 3000);
 
- app.listen(app.get('port'), () =>{
-    console.log('Server started on port: ', app.get('port'))
- });
+app.set('port',process.env.PORT || 3000);
+
+app.listen(app.get('port'), () =>{
+console.log('Server started on port: ', app.get('port'))
+
+});
+
