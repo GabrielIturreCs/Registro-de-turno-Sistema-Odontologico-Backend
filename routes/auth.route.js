9149df7e4525d6d348e53fa4b863cd264cb24b40
Lautaro <barrientoslautaro8@gmail.com>
2025-06-26 14:54:34 -0300

    feat(routes): Agrega rutas para login con Google y obtener perfil con middleware de autenticación [Barrientos Lautaro]

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router(); 
+const { verifyToken } = require('../middleware/auth.middleware');
 
 //defino controlador para el manejo de CRUD 
 const authCtrl = require('../controllers/auth.controller'); 
 
 // definiendo rutas 
-router.post('/', authCtrl.registerUsuario); 
-router.post('/login', authCtrl.loginUsuario); 
+router.post('/register', authCtrl.registerUsuario); 
+router.post('/login', authCtrl.loginUsuario);
+router.get('/confirmar-email', authCtrl.confirmarEmail); 
 
+router.get('/profile', verifyToken, authCtrl.getProfile);
+router.post('/google-login', authCtrl.googleLogin);
 //exportacion del modulo de rutas 
 module.exports = router;
