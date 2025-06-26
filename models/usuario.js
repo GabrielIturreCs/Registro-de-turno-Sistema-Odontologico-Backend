7567cc96a87ca7030708df83aad41ac3e863089c
Lautaro <barrientoslautaro8@gmail.com>
2025-06-26 14:53:24 -0300

    feat(usuario): Agrega campo googleId y ajusta lógica de requerimiento de contraseña en el modelo de usuario [Barrientos Lautaro]

--- models/usuario.js
+++ models/usuario.js
@@ -1,10 +1,30 @@
+const e = require("express");
 const mongoose = require("mongoose");
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
-const UsuarioSchema = new Schema({ 
-    nombreUsuario: {type: String, required: true}, 
-    password: {type:String, required:true},
-    tipoUsuario: {type:String, required: true}
- });
+const UsuarioSchema = new Schema({
+  nombreUsuario: { type: String, required: true },
+  password: {
+    type: String,
+    required: function () {
+      return !this.googleId;
+    },
+    select: false,
+  },
+  googleId: {
+    type: String,
+    unique: true,
+    sparse: true,
+  },
+  nombre: { type: String, required: true },
+  apellido: { type: String, required: false },
+  telefono: { type: String, required: true },
+  direccion: { type: String, required: true },
+  dni: { type: String, required: true },
+  tipoUsuario: { type: String, required: true },
+  email: { type: String, required: true },
+  isConfirmed: { type: Boolean, default: false },
+  confirmationToken: { type: String, default: null },
+});
 
- module.exports = mongoose.model('Usuario', UsuarioSchema);
+module.exports = mongoose.model("Usuario", UsuarioSchema);
