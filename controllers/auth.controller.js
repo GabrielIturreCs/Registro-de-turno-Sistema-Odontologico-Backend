5f9784a97d3fbcb44d4c88b54f7b092b925a9fa1
smrodriguez017 <39234551@fi.unju.edu.ar>
2025-07-01 16:01:14 -0300

    registrar usuario y  guardar datos  en colecciones según   tipo  de usuario(paciente, dentista, administrador)

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,55 +1,80 @@
 const Usuario = require ('../models/usuario') 
 const {encrypt,compare} = require('../helpers/handleBcrypt') 
 const authCtrl = {}
+const Paciente = require('../models/paciente');
+const Dentista = require('../models/dentista');
+const Administrador = require('../models/administrador');
 
 
 authCtrl.registerUsuario = async (req, res) => {
     try {
-        // Desestructurar el campo clave del cuerpo
-        const { nombreUsuario,password,confirmPassword,nombre,apellido,telefono,direccion,dni,tipoUsuario} = req.body;
+        console.log("BODY RECIBIDO:", req.body);
+        const { nombreUsuario, password, confirmPassword, nombre, apellido, telefono, direccion, dni, tipoUsuario, obraSocial } = req.body;
 
-        // Verificar si ya existe un usuario con ese email
-        /*const usuarioExiste = await Usuario.findOne({ email });
-        if (usuarioExiste) {
-            return res.status(400).json({
-                status: '0',
-                msg: 'El usuario ya existe con ese correo electrónico.'
-            });
-        }*/
         if (password !== confirmPassword) {
-              return res.status(400).json({ msg: 'Las contraseñas no coinciden.' });
+            return res.status(400).json({ msg: 'Las contraseñas no coinciden.' });
         }
-        // Encriptar la contraseña
+
         const passwordHash = await encrypt(password);
 
-        // Crear el nuevo usuario con la clave encriptada
+        // Solo los campos generales en Usuario
         const usuario = new Usuario({
             nombreUsuario,
-            password:passwordHash ,
-            nombre,
-            apellido,
-            telefono,
-            direccion,
-            dni,
+            password: passwordHash,
             tipoUsuario
-           
         });
 
-        // Guardar en la base de datos
         await usuario.save();
+
+        // Guardar en la colección correspondiente
+        if (tipoUsuario === 'paciente') {
+            if (!obraSocial) {
+                return res.status(400).json({ msg: 'Falta obra social para paciente.' });
+            }
+            const paciente = new Paciente({
+                nombre,
+                apellido,
+                telefono,
+                direccion,
+                dni,
+                userId: usuario._id,
+                obraSocial
+            });
+            await paciente.save();
+        } else if (tipoUsuario === 'dentista') {
+            const dentista = new Dentista({
+                nombre,
+                apellido,
+                telefono,
+                direccion,
+                dni,
+                userId: usuario._id
+            });
+            await dentista.save();
+        }else if (tipoUsuario === 'administrador') {
+            const administrador = new Administrador({
+                nombre,
+                apellido,
+                telefono,
+                direccion,
+                dni,
+                userId: usuario._id
+            });
+            await administrador.save();
+        }
 
         res.status(200).json({
             status: '1',
             msg: 'Usuario guardado.'
         });
     } catch (error) {
+        console.error("ERROR REGISTRO:", error); 
         res.status(400).json({
             status: '0',
             msg: 'Error procesando operación.'
         });
     }
 }
-
 authCtrl.loginUsuario = async (req, res) => { 
 
     try { 
@@ -83,9 +108,11 @@ authCtrl.loginUsuario = async (req, res) => {
             id: user._id 
         }) 
     } catch (error) { 
+        console.error("ERROR LOGIN:", error);
         res.json({ 
             status: 0, 
-            msg: 'error' 
+            msg: 'error' ,
+            error: error.message 
         }) 
     } 
 }
