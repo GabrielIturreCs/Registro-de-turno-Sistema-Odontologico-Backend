d760c202a10dce1715408ceca5a0e081f81e6d6f
Ivo <ivothaielvicencio@gmail.com>
2025-07-04 02:00:40 -0300

    mejorar la gestión de pagos y suscripciones en Mercado Pago, actualizando el controlador y las rutas

--- routes/mp.route.js
+++ routes/mp.route.js
@@ -2,8 +2,12 @@ const express = require('express')
 const router = express.Router();
 const mpCtrl = require('../controllers/mp.controller.js')
 
-
+// Definimos las rutas para la gestión de un pago único en mercado pago
 router.post('/payment', mpCtrl.getPaymentLink);
+
+// Definimos las rutas para la gestión de un pago por suscripción en mercado pago
+// ej. pagar todos los meses $ 10.000
+router.post('/subscription', mpCtrl.getSubscriptionLink);
 
 
 module.exports = router;
