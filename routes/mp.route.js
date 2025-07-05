9f20eda4a56fa9126074080340d99b75136f5416
Ivo <ivothaielvicencio@gmail.com>
2025-07-05 20:11:05 -0300

    refactor: Simplifique las rutas de pago y suscripción utilizando directamente los métodos del controlador

--- routes/mp.route.js
+++ routes/mp.route.js
@@ -1,25 +1,10 @@
-const express = require('express');
+const express = require('express')
 const router = express.Router();
-const mpCtrl = require('../controllers/mp.controller.js');
+const mpCtrl = require('../controllers/mp.controller.js')
 
-console.log('mpCtrl:', mpCtrl);
-console.log('mpCtrl keys:', Object.keys(mpCtrl));
 
-// Rutas básicas sin validación estricta
-router.post('/payment', (req, res) => {
-    if (mpCtrl.getPaymentLink) {
-        return mpCtrl.getPaymentLink(req, res);
-    } else {
-        return res.status(500).json({ error: 'getPaymentLink not available' });
-    }
-});
+router.post('/payment', mpCtrl.getPaymentLink);
+router.post('/subscription', mpCtrl.getSubscriptionLink);
 
-router.post('/subscription', (req, res) => {
-    if (mpCtrl.getSubscriptionLink) {
-        return mpCtrl.getSubscriptionLink(req, res);
-    } else {
-        return res.status(500).json({ error: 'getSubscriptionLink not available' });
-    }
-});
 
 module.exports = router;
